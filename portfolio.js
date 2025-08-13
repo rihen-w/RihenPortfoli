6ffb040c2f420d1a3c3b5ea91955af544b948669
Rihen w <rihenrw@Rihens-MacBook-Air.local>
2025-08-13 21:28:21 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+function skill(){
+    document.getElementById("skillcon").style.display="block"
+    document.getElementById("experianscon").style.display="none"
+    document.getElementById("educationcon").style.display="none"
+}
+function experience(){
+    document.getElementById("experianscon").style.display="block"
+    document.getElementById("skillcon").style.display="none"
+     document.getElementById("educationcon").style.display="none"
+}
+function education(){
+    document.getElementById("educationcon").style.display="block"
+    document.getElementById("skillcon").style.display="none"
+    document.getElementById("experianscon").style.display="none"
+}
+
+    
+   
+     
